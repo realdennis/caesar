d8bcceb1bfb71d94f3c8a55add3bbe665b61ee61
realdennis <realdennis777@gmail.com>
2019-12-27 01:16:34 +0800

    Let the generate type make sense

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,8 +1,8 @@
-import caesar from '../index';
-import '@testing-library/jest-dom/extend-expect';
+import caesar from "../index";
+import "@testing-library/jest-dom/extend-expect";
 
-const getElement = () => {
-  const el = document.createElement('div');
+const getElement: () => HTMLElement = () => {
+  const el = document.createElement("div");
   // Since jsdom does not support correct setProperty & getProperty implement
   // https://github.com/jsdom/jsdom/issues/1895
   (el as any).__temp__css__var = {};
@@ -10,23 +10,23 @@ const getElement = () => {
     (el as any).__temp__css__var = { [key]: value };
   };
   el.style.getPropertyValue = key => {
-    return (el as any).__temp__css__var[key] || ''; // If property did not exist, it should return empty string instead of undefined.
+    return (el as any).__temp__css__var[key] || ""; // If property did not exist, it should return empty string instead of undefined.
   };
   return el;
 };
 
-test('Assign should be work', () => {
+test("Assign should be work", () => {
   const el = getElement();
-  expect(el.style.getPropertyValue('--foo')).toBe('');
+  expect(el.style.getPropertyValue("--foo")).toBe("");
   caesar.assign(el, {
-    foo: 'bar'
+    foo: "bar"
   });
-  expect(el.style.getPropertyValue('--foo')).toBe('bar');
+  expect(el.style.getPropertyValue("--foo")).toBe("bar");
 });
 
-test('Query should be work', () => {
+test("Query should be work", () => {
   const el = getElement();
-  el.style.setProperty('--foo', 'bar');
-  const { foo } = caesar.query(el, ['foo']);
-  expect(foo).toBe('bar');
+  el.style.setProperty("--foo", "bar");
+  const { foo } = caesar.query(el, ["foo"]);
+  expect(foo).toBe("bar");
 });
